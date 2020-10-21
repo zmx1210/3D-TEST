98cd1496e0536952dc353fb83980a84236526375
章曼霞 <zhangmanxia@uinnova.com>
2020-10-21 15:43:09 +0800

    feat：初始化

---
+++ public/js/config.js
@@ -0,0 +1,13 @@
+/* eslint no-unused-vars: 'off' */
+const uino = {};
+const CONFIG = {
+    remote: 'http://39.106.83.131:9993',
+};
+uino.opensource_CAMERA = {
+    heading: -16.459116161460685,
+    height: 11857.787958673202,
+    lonlat: [117.29472624302355, 31.68370304145958, -0.0031710462644696236],
+    pitch: 57.09370887146087,
+    tilt: 57.09370887146087,
+    time: 2000,
+};
